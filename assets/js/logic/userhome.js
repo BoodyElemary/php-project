3804131376c9f250829bf0c9860b79203500e9ad
BoodyElemary <abdelrahman_elemary99@outlook.com>
2023-01-13 14:59:10 +0200

    add data to cart

--- assets/js/logic/userhome.js
+++ assets/js/logic/userhome.js
@@ -62,7 +62,8 @@ function fillProducts(productsArr) {
       "btn-sm",
       "mb-0",
       "toast-btn",
-      "customAlert"
+      "customAlert",
+      "addToCart"
     );
     addToCartBtn.innerText = "Add To Cart";
     addToCartBtn.dataset.target = "#infoToast";
@@ -82,18 +83,17 @@ function fillProducts(productsArr) {
     divParent.appendChild(divContainer);
     productsZone.appendChild(divParent);
   }
-
-  showToast();
+  addToCart();
 }
 // set this function for genertaed dom by js
 function showToast() {
-  let btns = document.getElementsByClassName("customAlert");
+  let btns = document.getElementsByClassName("addToCart");
   let infoToastDiv = document.getElementById("infoToast");
   for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener("click", () => {
-      console.log(infoToastDiv);
+      // console.log(infoToastDiv);
       infoToastDiv.classList.add("show");
-      console.log(infoToastDiv);
+      // console.log(infoToastDiv);
       setTimeout(() => {
         infoToastDiv.classList.remove("show");
       }, 1200);
@@ -101,86 +101,56 @@ function showToast() {
   }
 }
 
-{
-  /* <div class="col-xl-3 col-md-6 mb-xl-0 mb-4 mt-4">
-                    <div class="card card-blog card-plain">
-                      <div class="card-header p-0 mt-n4 mx-3">
-                        <a class="d-block shadow-xl border-radius-xl">
-                          <img src="https://ibc-static.broad.msu.edu/sites/globaledge/blog/55607.jpg" alt="img-blur-shadow" class="img-fluid shadow border-radius-xl">
-                        </a>
-                      </div>
-                      <div class="card-body p-3">
-                        <div class="d-flex align-items-center justify-content-between">
-                          <div>
-                            <h5>Coffe Late</h5>
-                            <div class="tags mb-2">
-                              <span class="badge bg-gradient-info">Hot Drink</span>
-                            </div>
-                          </div>
-                          <span class="fs-4">450 EGP</span>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-end gap-3">
-                          <button type="button" class="btn btn-primary btn-sm mb-0 toast-btn" data-target="infoToast">
-                            add to cart
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div> */
+//send datat to cart<===================================
+let addedProducts = [];
+function addToCart() {
+  let addBtns = document.getElementsByClassName("addToCart");
+  for (let i = 0; i < addBtns.length; i++) {
+    addBtns[i].addEventListener("click", (e) => {
+      let data = {};
+      let clicked = e.target;
+      let parent = clicked.parentElement.parentElement;
+      let orderdProductName = parent.firstChild.firstChild.firstChild.innerText;
+      let orderdProductPrice = parent.firstChild.lastChild.innerText;
+      data["orderdProductName"] = orderdProductName;
+      data["orderdProductPrice"] = orderdProductPrice;
+      addedProducts.push(data);
+      // console.log(addedProducts);
+      fillProductCart(addedProducts);
+    });
+  }
+  showToast();
 }
+
+//add items to cart <====================================
+
+// let CartDivContainer = document.getElementById("cartParent");
+// console.log(CartDivContainer);
+function fillProductCart(addedProducts) {
+  let cartContainer = document.getElementById("cartParent");
+   
+
+  console.log(cartContainer);
+}
+
+<div class="d-flex flex-column mt-2 justify-content-center align-items-center">
+              <div class="d-flex justify-content-center align-items-baseline col-12">
+                <div class="col-5"><h6>ice espresso</h6></div>
+                <div class="input-group input-group-outline mx-1">
+                  <input type="number" class="form-control fw-bolder text-warning" onfocus="focused(this)" onfocusout="defocused(this)">
+                </div>
+                <div class="col-3 mx-2"><h6>EGP 2225</h6></div>
+                <div class="col-1">
+                  <button class="btn btn-link text-danger p-0 text-center mt-2 mx-1 fw-bolder">
+                    <i class="material-icons">clear</i>
+                  </button>
+                </div>
+              </div>
+            </div>
+
 
 // addToCart();
 // viewProduct();
-
-{
-  /* <div class="card card-blog card-plain">
-  {" "}
-  // container
-  <div class="card-header p-0 mt-n4 mx-3">
-    {" "}
-    // divheader
-    <a class="d-block shadow-xl border-radius-xl">
-      {" "}
-      /imglink
-      <img
-        src="https://ibc-static.broad.msu.edu/sites/globaledge/blog/55607.jpg" //img
-        alt="img-blur-shadow"
-        class="img-fluid shadow border-radius-xl"
-      />
-    </a>
-  </div>
-  <div class="card-body p-3">
-    {" "}
-    // cardbody
-    <div class="d-flex align-items-center justify-content-between">
-      {" "}
-      //cardbodycontainer
-      <div>
-        {" "}
-        // divnameinfo
-        <h5>Coffe Late</h5> // h5name
-        <div class="tags mb-2">
-          {" "}
-          //divcatinfo
-          <span class="badge bg-gradient-info">Hot Drink</span> //spancat
-        </div>
-      </div>
-      <span class="fs-4">450 EGP</span> //spanprice
-    </div>
-    <div class="d-flex align-items-center justify-content-end gap-3">
-      {" "}
-      //addtocartcontainer
-      <button
-        type="button"
-        class="btn btn-primary btn-sm mb-0 toast-btn" // addtocartbtn
-        data-target="infoToast"
-      >
-        add to cart
-      </button>
-    </div>
-  </div>
-</div>; */
-}
 
 ///////////////// fetch func. ////////////////////////////////
 async function get_products() {
