992f38ab3dc49811ffceefd96be971dea8481625
MahmoudElfakharany <m.a.el-fakharany@hotmail.com>
2023-01-18 04:48:47 +0200

    orders page almost done

--- assets/js/logic/orders.js
+++ assets/js/logic/orders.js
@@ -1,115 +1,177 @@
-///////////////// GRABBING ELEMENTS /////////////////////////////
-// ===== parent elements to append to:
-let userNameParent = document.getElementById("userNameParent");
-let orderDateParent = document.getElementById("orderDateParent");
-let roomNumberParent = document.getElementById("roomNumberParent");
-// ===== order info:
-let userName_field = document.getElementById("userName");
-let orderDate_field = document.getElementById("orderDate");
-let roomNumber_field = document.getElementById("roomNumber");
-let orderStatus_field = document.getElementById("orderStatus");
-// ===== card info :
-let productCard_container = document.getElementById("wholeCard_container");
-let upperCard_div = document.getElementById("upperCard_div");
-let productImage_field = document.getElementById("productImage");
-let productCount_field = document.getElementById("productCount");
-//---
-let bottomCard_div = document.getElementById("bottomCard_div");
-let productName_field = document.getElementById("productName");
-let productCategory_field = document.getElementById("productCategory");
-let productPrice_field = document.getElementById("productPrice");
-
-
-
-
-
-
-
-
-
+// ===== parent element to append to:
+let wholeOrderDiv = document.getElementById("wholeOrderDiv");
+/*
+ *
+ */
 
 ///////////////////////////////////// >>> FILL ORDER FIELD <<< /////////////////////////////
+let product_loop_begin = 0; // index to start looping for product on
 
 function fill_Orders(OrdersArray) {
-  for (let i = 0; i < OrdersArray.length; i++) {
+  //----- outer loop to creat orders , inner loop to get ordered products
+  for (let i = 0; i < OrdersArray["orders"].length; i++) {
+    let product_card_array = []; // insert products here
+    product_card_array.length = 0;
+    for (
+      let j = product_loop_begin;
+      j < product_loop_begin + Number(OrdersArray["orders"][i]["no_products"]);
+      j++
+    ) {
+      product_card_array.push(`
+        <div class="card-body px-0 pb-2 d-flex">
+        <div class="row">
+        <div class="col-xl-3 col-md-6 mb-xl-0 mb-4 mt-4">
+        <div class="card card-blog card-plain w-md-75">
+        <div class="card-header p-0 mt-n4 mx-3" onclick=alerrt()>
+        <a class="d-block shadow-xl border-radius-xl">
+        <img
+        src="${OrdersArray["products"][j]["product_picture"]}"
+        alt="img-blur-shadow"
+        class="img-fluid shadow border-radius-xl position-relative"
+        />
+                        <span
+                        class="position-absolute top-20 start-75 translate-middle badge rounded-pill bg-warning p-2"
+                        >
+                        x <span id="Itemscount">${OrdersArray["products"][j]["quantity"]}</span>
+                        <span class="visually-hidden">unread messages</span>
+                        </span>
+                        </a>
+                        </div>
+                        <div class="card-body p-3">
+                        <div
+                        class="d-flex align-items-center justify-content-between"
+                        >
+                        <div>
+                        <h5>${OrdersArray["products"][j]["product_name"]}</h5>
+                        <div class="tags mb-2">
+                        <span class="badge bg-gradient-info"
+                        >${OrdersArray["products"][j]["category_name"]}</span
+                        >
+                        </div>
+                        </div>
+                        <span class="fs-4">${OrdersArray["products"][j]["product_price"]}</span>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        <!-- here we can add products -->
+                        </div>
+                        </div>`);
+    }
+    product_loop_begin =
+      product_loop_begin + Number(OrdersArray["orders"][i]["no_products"]);
 
-    let userName = document.createElement("h6"); // user name field
-    let orderDate = document.createElement("h6"); // order date field
+    //----- stating the status of the order
+    let selected1 = "";
+    let selected2 = "";
+    let selected3 = "";
+    if (OrdersArray["orders"][i]["order_status"] == "progress") {
+      selected1 = "selected";
+    } else if (OrdersArray["orders"][i]["order_status"] == "out for delivery") {
+      selected2 = "selected";
+    } else {
+      selected3 = "selected";
+    }
 
-    
-    let divHeader = document.createElement("div"); ////
-    let imglink = document.createElement("a"); ////
-    let img = document.createElement("img"); /////
-    let divCardBody = document.createElement("div"); ////
-    let cardBodyContainer = document.createElement("div"); ////
-    let divNameInfo = document.createElement("div"); ////
-    let h5Name = document.createElement("h5"); ////
-    let divCatInfo = document.createElement("div"); ////
-    let spanCat = document.createElement("span"); ////
-    let spanPrice = document.createElement("span"); ////
-    let addToCartContainer = document.createElement("div"); ////
-    let addToCartBtn = document.createElement("button"); ////
+    //----- creating orders
+    wholeOrderDiv.innerHTML += `      <div class="row">
+    <div class="col-12">
+      <div class="card my-4"> 
+      <div class="card">
+      <div
+      class="card-header p-0 position-relative mt-n4 mx-3 z-index-2"
+          >
+          <div
+              class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3"
+            >
+              <h6 class="text-white text-capitalize ps-3">
+              ${OrdersArray["orders"][i]["user_name"]} Orders
+              </h6>
+            </div>
+          </div>
+          <div class="card-body px-0 pb-2">
+            <div class="table-responsive p-0">
+              <table class="table align-items-center mb-0">
+                <thead>
+                  <tr>
+                    <th
+                      class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+                    >
+                      <h6>Order Date</h6>
+                    </th>
+                    <th
+                      class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"
+                    >
+                      <h6>Name</h6>
+                    </th>
+                    <th
+                      class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"
+                    >
+                      <h6>Room</h6>
+                    </th>
+                    <th
+                      class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"
+                    >
+                      <h6>Action</h6>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <div class="d-flex px-2 py-1">
+                        <div class="d-flex px-2 py-1">
+                          <div
+                            class="d-flex flex-row justify-content-center align-items-baseline"
+                          >
+                            <h6 class="mb-0 text-sm">
+                            ${OrdersArray["orders"][i]["order_date"]}
+                            </h6>
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      <p class="text-xs font-weight-bold mb-0">
+                        EGP <span id="orderAmount"> ${OrdersArray["orders"][i]["total"]} </span>
+                      </p>
+                    </td>
 
-    divParent.classList.add("col-xl-3", "col-md-6", "mb-xl-0", "mb-4", "mt-4");
-    divContainer.classList.add("card", "card-blog", "card-plain");
-    divHeader.classList.add("card-header", "p-0", "mt-n4", "mx-3");
-    imglink.classList.add("d-block", "shadow-xl", "border-radius-xl");
-    img.setAttribute("src", `${OrdersArray[i]["product_picture"]}`);
-    img.classList.add("img-fluid", "shadow", "border-radius-xl");
-    divCardBody.classList.add("card-body", "p-3");
-    cardBodyContainer.classList.add(
-      "d-flex",
-      "align-items-center",
-      "justify-content-between"
-    );
-    h5Name.innerText = `${OrdersArray[i]["product_name"]}`;
-    divCatInfo.classList.add("tags", "mb-2");
-    spanCat.classList.add("badge", "bg-gradient-info");
-    spanCat.innerText = `${OrdersArray[i]["category_name"]}`;
-    spanPrice.classList.add("fs-4", "product-price");
-    spanPrice.innerText = `${OrdersArray[i]["product_price"]}`;
-    addToCartContainer.classList.add(
-      "d-flex",
-      "align-items-center",
-      "justify-content-end",
-      "gap-3"
-    );
-    addToCartBtn.setAttribute("type", "button");
-    addToCartBtn.classList.add(
-      "btn",
-      "btn-primary",
-      "btn-sm",
-      "mb-0",
-      "toast-btn",
-      "customAlert",
-      "addToCart"
-    );
-    addToCartBtn.innerText = "Add To Cart";
-    addToCartBtn.dataset.target = "#infoToast";
-    divContainer.appendChild(divHeader);
-    divContainer.appendChild(divCardBody);
-    divContainer.appendChild(addToCartContainer);
-    divHeader.appendChild(imglink);
-    imglink.appendChild(img);
-    divCardBody.appendChild(cardBodyContainer);
-    divCardBody.appendChild(addToCartContainer);
-    cardBodyContainer.appendChild(divNameInfo);
-    cardBodyContainer.appendChild(spanPrice);
-    divNameInfo.appendChild(h5Name);
-    divNameInfo.appendChild(divCatInfo);
-    divCatInfo.appendChild(spanCat);
-    addToCartContainer.appendChild(addToCartBtn);
-    divParent.appendChild(divContainer);
-    productsZone.appendChild(divParent);
+                    <td>
+                      <p class="text-xs font-weight-bold mb-0">
+                        <span>${OrdersArray["orders"][i]["room_no"]}</span>
+                      </p>
+                    </td>
+
+                    <td>
+                      <div class="input-group input-group-outline w-50">
+                        <select
+                          class="form-control fw-bolder text-warning"
+                          id="statuSelection"
+                          style="border-radius: 0 !important"
+                          aria-placeholder="select-user"
+                          onchange=lolo()
+                        >
+                          <option value="progress" ${selected1}>progress</option>
+                          <option value="out for delivery" ${selected2}>out for delivery</option>
+                          <option value="delivered" ${selected3}>delivered</option>
+                        </select>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>  
+              </table>
+            </div>
+          </div>
+        </div>
+        ${product_card_array}
+      </div>
+    </div>
+  </div>`;
   }
-  addToCart();
 }
 
-
-
-
-
-
+//------ receving and sending function
 async function get_Orders() {
   let response = await fetch("http://localhost/php-project/server/Orders.php", {
     method: "GET",
@@ -117,12 +179,22 @@ async function get_Orders() {
     // headers:
   });
   let data = await response.json();
-  console.log(data);
-  // return data;
+  // console.log(data);
 
+  fill_Orders(data);
+  // fill_cards(data["products"]);
   // if(data['status']==true){
   //             // window.location="./profile.html"
   //             window.open("http://localhost/lab_3/profile.html","_self");
   //         }
 }
 get_Orders();
+
+function alerrt() {
+  alert("ttttttttt");
+}
+
+function lolo() {
+  for (let i = 0; i < statuSelection.length; i++)
+    alert(statuSelection[i].value);
+}
--- server/Orders.php
+++ server/Orders.php
@@ -3,20 +3,46 @@
 require("./env.php");
 
 
-
 ///////////////// get products ////////////////////////////////
 $sql = 'mysql:host=localhost;dbname=cafeteriadb';
 
-$query = "SELECT u.user_name , o.room_no , o.order_date ,o.order_status , o.total , op.quantity , p.product_name , p.product_picture , p.product_price 
-FROM orders o join users u on o.user_id = u.user_id join orderedproducts op on o.order_id = op.order_id join products p on op.product_id = p.product_id";
-$sql = $conn->prepare($query);
+//------ getting ifo of orderss
+$order_query = "SELECT u.user_name , o.room_no , o.order_date ,o.order_status , o.total , count(op.product_id) as no_products
+ FROM orders o join users u on o.user_id = u.user_id join orderedproducts op on o.order_id = op.order_id
+  join products p on op.product_id = p.product_id join category c on p.category_id = c.category_id group by o.order_id order by o.order_id;";
+ 
+ // ------ getting info of ordered products
+$product_query = "SELECT  op.quantity , p.product_name , p.product_picture , p.product_price , c.category_name
+FROM orders o join users u on o.user_id = u.user_id join orderedproducts op on o.order_id = op.order_id 
+join products p on op.product_id = p.product_id join category c on p.category_id = c.category_id order by o.order_id;";
+
+$sql = $conn->prepare($order_query);
 $sql->execute();
 $result = $sql->execute();
+$orders = $sql->fetchAll(PDO::FETCH_ASSOC);
 
+$sql = $conn->prepare($product_query);
+$sql->execute();
+$result = $sql->execute();
+$products = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+//------ creating array that hold all
+$all=[
+    "orders"=>$orders,
+    "products"=>$products
+];
+
+
+
+//----------- sending response
 if ($result) {
-    $product = $sql->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($product);
+    echo json_encode($all);
 }
-// print_r($product);
+else 
+{
+    $error = [
+        "error"=>true
+    ];
+    echo json_encode($error);
+}
 
-// echo "dddddddddddddddddddd";
